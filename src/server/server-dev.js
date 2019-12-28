372ca912805ed0c3def914178d305581d29b5bab
shobhit-singh <shobhitsingh29@gmail.com>
2019-12-28 08:13:22 +0530

    Updates

---
+++ src/server/server-dev.js
@@ -0,0 +1,99 @@
+import path from 'path'
+import express from 'express'
+import webpack from 'webpack'
+import mongoose from "mongoose";
+import getSecret from "./secret";
+import bodyParser from "body-parser";
+import Data from "./data";
+
+import webpackDevMiddleware from 'webpack-dev-middleware'
+import webpackHotMiddleware from 'webpack-hot-middleware'
+import config from '../../webpack.dev.config.js'
+
+
+mongoose.connect(getSecret("dbUri"));
+let db = mongoose.connection;
+db.on("error", console.error.bind(console, "MongoDB connection error:"));
+
+const router = express.Router();
+
+const app = express(),
+            DIST_DIR = __dirname,
+            HTML_FILE = path.join(DIST_DIR, 'index.html'),
+            compiler = webpack(config)
+
+app.use(webpackDevMiddleware(compiler, {
+  publicPath: config.output.publicPath
+}));
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use(webpackHotMiddleware(compiler))
+app.use("/api", router);
+
+
+router.get("/getData", (req, res) => {
+  Data.find((err, data) => {
+    if (err) return res.json({ success: false, error: err });
+    return res.json({ success: true, data: data });
+  });
+});
+
+router.post("/updateData", (req, res) => {
+  const { id, update } = req.body;
+  Data.findByIdAndUpdate(id, update, err => {
+    if (err) return res.json({ success: false, error: err });
+    return res.json({ success: true });
+  });
+});
+
+router.delete("/deleteData", (req, res) => {
+  const { id } = req.body;
+  Data.findByIdAndRemove(id, err => {
+    if (err) return res.send(err);
+    return res.json({ success: true });
+  });
+});
+
+router.post("/putData", (req, res) => {
+  let data = new Data();
+
+  const { id, message } = req.body;
+
+  if ((!id && id !== 0) || !message) {
+    return res.json({
+      success: false,
+      error: "INVALID INPUTS"
+    });
+  }
+  data.message = message;
+  data.id = id;
+  data.save(err => {
+    if (err) return res.json({ success: false, error: err });
+    return res.json({ success: true });
+  });
+});
+
+
+
+router.get("/", (req, res) => {
+  res.json({ message: "HELLOW WORLDUUHHHH" });
+});
+
+app.get('*', (req, res, next) => {
+  compiler.outputFileSystem.readFile(HTML_FILE, (err, result) => {
+    if (err) {
+      return next(err)
+    }
+    res.set('content-type', 'text/html')
+    res.send(result)
+    res.end()
+  })
+})
+const PORT = process.env.PORT || 8080
+
+app.listen(PORT, () => {
+    console.log(`App listening to ${PORT}....`)
+    console.log('Press Ctrl+C to quit.')
+})
