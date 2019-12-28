6b6ae8e29f2495bb95db13756676b48ebe7182f3
shobhit-singh <shobhitsingh29@gmail.com>
2019-12-28 09:05:36 +0530

    Updates

--- src/server/secret.js
+++
@@ -1,7 +0,0 @@
-const secrets = {
-  dbUri: "mongodb+srv://shobhitsingh29:yatra@clusterbackend-nypki.mongodb.net/shobhitdb"
-};
-
-const getSecret = key => secrets[key];
-
-export default getSecret;
